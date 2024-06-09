b9765d35499d40a5e264572cbc9d17b76de02c33
test user <test.user@company.com>
2024-06-09 14:27:07 +0100

    added info page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return "<h1>Harveys Simple Flask App v2</h1>"
 
+@app.route('/info')
+def info():
+    return "<h2>Harveys Flask App Info Page</h2>"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
     
