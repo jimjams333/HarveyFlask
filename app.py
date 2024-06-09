8ce28b335a87c4ed579e732203a858b14f117ff0
test user <test.user@company.com>
2024-06-09 14:22:56 +0100

    feature branch v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Harveys Simple Flask App</h1>"
+    return "<h1>Harveys Simple Flask App v2</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
